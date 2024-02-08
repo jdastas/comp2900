b706a911df9736c23af0a144fe8a444c9724c24c
Javier A Dastas <jdastas@arecibo.inter.edu>
2024-02-08 13:48:39 -0400

    Update my files

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,2 +1,8 @@
 print('Hola Mundo!')
-print('John Doe')
+mensaje = 'John Doe' # String value
+gpa = 3.14  # number (real) value
+isValid = True # boolean value
+print(mensaje)
+print(gpa)
+print(isValid)
+print(f'{mensaje} tu GPA es {gpa:5.2}')
---
+++ modulo01/lab02/app.py
@@ -0,0 +1,5 @@
+mensaje = input('Entre su nombre: ')
+gpa = float(input('Entre su GPA: '))
+edad = int(input('Ingrese su edad: '))
+
+print(f'Hola {mensaje}!!!, tu GPA es {gpa}')
