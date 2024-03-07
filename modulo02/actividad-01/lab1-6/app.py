3cc5b593463d778f2942b22789e252f1013478e3
Javier A Dastas <jdastas@arecibo.inter.edu>
2024-03-07 14:00:20 -0400

    Update modulo 2

---
+++ modulo02/actividad-01/lab1-6/app.py
@@ -0,0 +1,12 @@
+import array
+
+def invertir_cadena(cadena):
+    array_length = len(cadena)
+    nueva_cadena = ''
+    for i in range(array_length):        
+        nueva_cadena += (cadena[array_length-1-i] )
+    return nueva_cadena
+
+mensaje = "el oso peresozo"
+print(mensaje)
+print(invertir_cadena(mensaje))
