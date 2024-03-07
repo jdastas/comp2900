14bd643cc59aa21f7727cf6311e1feba3fff365c
Javier A Dastas <jdastas@arecibo.inter.edu>
2024-03-07 13:27:26 -0400

    Update modulo 2

---
+++ modulo02/actividad-01/lab1-4/app.py
@@ -0,0 +1,21 @@
+# Python program to get average of a list 
+def Average(lst): 
+    return sum(lst) / len(lst) 
+ 
+def promedio(lista_numeros):
+    suma = 0  
+    ctr = 0  
+    for valor in lista_numeros:
+        suma = suma + valor #   suma += valor
+        ctr = ctr + 1       #   ctr++
+    return (suma / len(lista_numeros))
+
+# Version ChatGPT
+lst = [15, 9, 55, 41, 35, 20, 62, 49] 
+average = Average(lst) 
+print(lst)
+print(f'El promedio de la lista es {average}')
+
+# Versioin del Curso
+print(lst)
+print(f'El valor promedio de la lista es {promedio(lst)}')
--- modulo02/arrays01/app.py
+++ modulo02/arrays01/app.py
@@ -1,11 +1,12 @@
 import array
 
-aNumber = [0] * 3
+# aNumber = [0] * 3
+aNumber = array.array('i',[])
 
 for i in range(1, 4): # from 1 to 3
     number = int(input('Enter a number: '))
-    aNumber[i-1] = number
-    # aNumber.append(number)
+    # aNumber[i-1] = number
+    aNumber.append(number)
 
 for i in range(3): # from 0 to 2
     print(f'Indice {i} - Valor {aNumber[i]}')
